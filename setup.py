c1e1075e1c980acee919a3456cae84181092d694
annagaines <gaines.annab@gmail.com>
2019-05-22 13:28:28 -0400

    Addiding

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+try:
+  import os
+  from setuptools import setup, find_packages
+except ImportError:
+  from distutils.core import setup
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md')) as fh:
+      long_description_text = fh.read()
+
+
+setup(
+  name = "CANDY",
+  scripts = ['candy.py'],
+  version = "0.1.0",
+  description = 'Database curation tool for targeted-amplicon sequencing.',
+  long_description=long_description_text,
+  long_description_content_type="text/markdown",
+  author = 'Anna Gaines',
+  author_email = 'anna.gaines@ihrc.com',
+  url = 'https://github.com/ar0ch/URDO-CANDy',
+  classifiers = [
+      'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['lxml'],
+)
